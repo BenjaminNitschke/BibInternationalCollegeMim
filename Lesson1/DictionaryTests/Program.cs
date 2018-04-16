801718698ed0f4f560749b827243b642368ea2d3
Benjamin <Benjamin@DeltaEngine.net>
2018-04-16 10:17:46 +0200

    Code Review

--- Lesson1/DictionaryTests/Program.cs
+++ Lesson1/DictionaryTests/Program.cs
@@ -1,50 +1,51 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using NUnit.Framework;
 
-namespace DicitionaryTests
+namespace DictionaryTests
 {
-    public class Program
-    {
-        public static void Main()
-        {
-            AssertResult(ToDictionary("a=1;b=2;c=3"), new[] { "1", "2", "3" });
-            AssertResult(ToDictionary("a=1;a=2"), new[] { "2" });
-            AssertResult(ToDictionary("a=1;b=2"), new[] { "1", "2" });
-            AssertResult(ToDictionary("a="), new[] { "" });
-            Assert.Throws<Exception>(() => AssertResult(ToDictionary("=1"), new[] { "" }));
-            AssertResult(ToDictionary(""), new string[] { });
-            AssertResult(ToDictionary("a==1"), new[] { "=1" });
-        }
+	public class Program
+	{
+		public static void Main()
+		{
+			AssertResult(ToDictionary("a=1;b=2;c=3"), new[] { "1", "2", "3" });
+			AssertResult(ToDictionary("a=1;b=2;d=3;e=11;f=83570892357"), new[] { "1", "2", "3", "11", "83570892357" });
+		
+			AssertResult(ToDictionary("a=1;a=2"), new[] { "2" });
+			AssertResult(ToDictionary("a=1;b=2"), new[] { "1", "2" });
+			AssertResult(ToDictionary("a="), new[] { "" });
+			Assert.Throws<Exception>(() => AssertResult(ToDictionary("=1"), new[] { "" }));
+			AssertResult(ToDictionary(""), new string[] { });
+			AssertResult(ToDictionary("a==1"), new[] { "=1" });
+		}
 
-        private static void AssertResult(IDictionary<string, string> result, string[] expectedValues)
-        {
-            Assert.That(result.Count, Is.EqualTo(expectedValues.Length));
-            var index = 0;
-            foreach (var item in result)
-                Assert.That(item.Value, Is.EqualTo(expectedValues[index++]));
-        }
+		private static void AssertResult(IDictionary<string, string> result, string[] expectedValues)
+		{
+			Assert.That(result.Count, Is.EqualTo(expectedValues.Length));
+			var index = 0;
+			foreach (var item in result)
+				Assert.That(item.Value, Is.EqualTo(expectedValues[index++]));
+		}
 
-        private static IDictionary<string, string> ToDictionary(string input)
-        {
-            var result = new Dictionary<string, string>();
+		private static IDictionary<string, string> ToDictionary(string input)
+		{
+			var result = new Dictionary<string, string>();
+			foreach (var part in input.Split(';'))
+				AddToDictionary(part, result);
+			return result;
+		}
 
-            if (input == "")
-                return result;
-
-            var parts = input.Split(';');
-            for (int i = 0; i < parts.Length; i++)
-            {
-                var entry = parts[i].Split(new[] { '=' }, 2);
-
-                if (entry[0] == "")
-                    throw new Exception("Empty Key");
-                else if (!result.ContainsKey(entry[0]))
-                    result.Add(entry[0], entry[1]);
-                else
-                    result[entry[0]] = entry[1];
-            }
-            return result;
-        }
-    }
+		private static void AddToDictionary(string part, Dictionary<string, string> result)
+		{
+			var entry = part.Split(new[] { '=' }, 2);
+			if (entry.Length != 2)
+				return;
+			if (entry[0] == "")
+				throw new Exception("Empty Key");
+			if (!result.ContainsKey(entry[0]))
+				result.Add(entry[0], entry[1]);
+			else
+				result[entry[0]] = entry[1];
+		}
+	}
 }
