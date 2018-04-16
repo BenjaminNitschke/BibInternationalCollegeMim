8a778839453b55bb3b214170392848bb18d22594
Benjamin <Benjamin@DeltaEngine.net>
2018-04-16 09:09:14 +0200

    Added line

--- Lesson1/DictionaryTests/Program.cs
+++ Lesson1/DictionaryTests/Program.cs
@@ -17,6 +17,7 @@ namespace DictionaryTests
 				Console.WriteLine(pair.Key + ": "+pair.Value);
 		}
 
+
 		public static IDictionary<string, string> ToDictionary(string input)
 		{
 			return new Dictionary<string, string>();
