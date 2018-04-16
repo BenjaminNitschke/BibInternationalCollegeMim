7327340f8d235b80455532bcefd0109774bd365a
Benjamin <Benjamin@DeltaEngine.net>
2018-04-16 08:09:58 +0200

    First week, Lesson1 for Dictionaries in C# and TextAdventure start in C++

---
+++ Lesson1/DictionaryTests/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryTests
+{
+	public class Program
+	{
+		public static void Main()
+		{
+
+			Console.WriteLine("DictionaryTestsrrqr");
+		}
+
+		public IDictionary<string, string> ToDictionary(string input)
+		{
+			return null;
+		}
+	}
+}
