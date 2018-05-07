d488dd0957b9a2e6c573556406a0f799cad3a6cc
MarvinKnoche <marvinknoche@t-online.de>
2018-05-07 11:05:45 +0200

    GameOfLife
    Gosper Glider Gun

---
+++ Lesson3/GameOfLife/GameOfLife/Game.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Game
+    {
+        private bool[,] currentMap;
+        private bool[,] newMap;
+
+        private int width;
+        private int height;
+
+        public Game() { }
+
+        public Game(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            currentMap = new bool[this.width, this.height];
+            newMap = new bool[this.width, this.height];
+        }
+
+        public void SetUp()
+        {
+            Random r = new Random();
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (r.NextDouble() < 0.3d) currentMap[x, y] = true;
+                    else currentMap[x, y] = false;
+                }
+            }
+        }
+
+        public void SetUp(bool[,] map, int xPos, int yPos)
+        {
+            for(int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    currentMap[xPos + x, yPos + y] = map[x, y];
+                }
+            }
+        }
+
+        public void Add(bool[,] map, int xPos, int yPos)
+        {
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    currentMap[xPos + x, yPos + y] = map[x, y];
+                }
+            }
+        }
+
+        public void Tick()
+        {
+            newMap = new bool[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    int neighbours = GetNeighbours(x, y);
+                    bool shouldLive = false;
+                    if (currentMap[x, y])
+                    {
+                        if (neighbours == 2 || neighbours == 3)
+                        {
+                            shouldLive = true;
+                        }
+                    }
+                    else
+                    {
+                        if (neighbours == 3)
+                        {
+                            shouldLive = true;
+                        }
+                    }
+                    newMap[x, y] = shouldLive;
+                }
+            }
+            currentMap = newMap;
+        }
+
+        private int GetNeighbours(int xCheck, int yCheck)
+        {
+            int neighbours = 0;
+            for (int x = -1; x < 2; x++)
+            {
+                for (int y = -1; y < 2; y++)
+                {
+                    int xPos = xCheck + x;
+                    int yPos = yCheck + y;
+                    if ((x != 0 || y != 0) && (0 <= xPos && xPos < width) && (0 <= yPos && yPos < height))
+                    {
+                        if (currentMap[xPos, yPos])
+                            neighbours++;
+                    }
+                }
+            }
+            return neighbours;
+        }
+
+        public void Draw()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (currentMap[x, y]) Console.Write("o ");
+                    else Console.Write("  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lesson3/GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading;
+using System;
+
+namespace GameOfLife
+{
+    public class Program
+    {
+        private int[,] gosperGliderGunTemp = new int[,]
+        {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+            { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        private bool[,] glider = new bool[,]
+        {
+                {false, true, false },
+                {false, false, true },
+                {true, true, true}
+        };
+
+        private bool[,] beacon = new bool[,]
+        {
+            { true, true, false, false},
+            { true, false, false, false},
+            { false, false, false, true},
+            { false, false, true, true},
+        };
+
+        private bool[,] gosperGliderGun;
+
+
+
+        private void StartGame()
+        {
+            gosperGliderGun = new bool[gosperGliderGunTemp.GetLength(0), gosperGliderGunTemp.GetLength(1)];
+            for(int i = 0; i < gosperGliderGunTemp.GetLength(0); i++)
+            {
+                for (int j = 0; j < gosperGliderGunTemp.GetLength(1); j++)
+                {
+                    if(gosperGliderGunTemp[i, j] == 1) gosperGliderGun[i, j] = true;
+                    else gosperGliderGun[i, j] = false;
+                }
+            }
+
+            Game game = new Game(50, 50);
+            game.Add(gosperGliderGun, 0, 0);
+            game.Draw();
+            bool running = true;
+            while (running)
+            {
+                Thread.Sleep(1);
+                Console.SetCursorPosition(0, 0);
+                game.Tick();
+                game.Draw();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.StartGame();
+        }
+    }    
+}
