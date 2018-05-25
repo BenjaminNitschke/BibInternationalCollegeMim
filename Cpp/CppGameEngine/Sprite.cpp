405e90b27b10b5cb4f0ffb0ce2eae4e1e4d2e9bf
JH <rebelhero51@posteo.de>
2018-05-25 08:51:00 +0200

    Nothing works
    Nothing works, I keep getting the "missing msvcr120d.dll" error, will switch to the version that is not from me

--- Cpp/CppGameEngine/Sprite.cpp
+++ Cpp/CppGameEngine/Sprite.cpp
@@ -10,12 +10,13 @@ void Sprite::Draw(float x, float y)
 	glEnable(GL_TEXTURE_2D);
 	glBegin(GL_QUADS);
 	glTexCoord2d(0, 0);
-	glVertex3f(initialX + x - width, initialY + y - height, 0);
-	glTexCoord2d(0, 1);
-	glVertex3f(initialX + x - width, initialY + y + height, 0);
-	glTexCoord2d(1, 1);
-	glVertex3f(initialX + x + width, initialY + y + height, 0);
+	glVertex3f(-1 + (initialX + x) * 2, -1 + (initialY + y) * 2, 0);
 	glTexCoord2d(1, 0);
-	glVertex3f(initialX + x + width, initialY + y - height, 0);
+	glVertex3f(-1 + (initialX + x + width) * 2, -1 + (initialY + y) * 2, 0);
+	glTexCoord2d(1, 1);
+	glVertex3f(-1 + (initialX + x + width) * 2, -1 + (initialY + y + height) * 2, 0);
+	glTexCoord2d(0, 1);
+	glVertex3f(-1 + (initialX + x) * 2, -1 + (initialY + y + height) * 2, 0);
+
 	glEnd();
 }
