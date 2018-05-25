5d145b2cbde0ec0607a8de13f4cf0480fd3c36dc
Jannis Kohlstrung <38348933+SirJannis@users.noreply.github.com>
2018-05-25 10:12:40 +0200

    Cleanup SpaceInvaders
    Cleaned up includes

--- Cpp/SpaceInvaders/Program.cpp
+++ Cpp/SpaceInvaders/Program.cpp
@@ -1,14 +1,13 @@
 #include "stdafx.h"
 #include "SpaceInvadersGame.h"
-#include <../glfw-3.2.1.bin.WIN64/include/GLFW/glfw3.h>
-#include "Game.h"
+
 using namespace CppGameEngine;
+
 FILE _iob[] = { *stdin, *stdout, *stderr };
 extern "C" FILE * __cdecl __iob_func(void)
 {
 	return _iob;
 }
-
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
