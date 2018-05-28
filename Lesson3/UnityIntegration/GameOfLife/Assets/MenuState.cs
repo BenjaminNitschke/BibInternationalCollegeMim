b3d7098d885fa81509849c67c4d0117e996feac1
Benjamin <Benjamin@DeltaEngine.net>
2018-05-28 11:22:35 +0200

    Class based Menu State Machine

--- Lesson3/UnityIntegration/GameOfLife/Assets/MenuState.cs
+++ Lesson3/UnityIntegration/GameOfLife/Assets/MenuState.cs
@@ -1,15 +1,63 @@
-﻿public enum MenuState
+﻿using UnityEngine.SceneManagement;
+
+public abstract class MenuState
 {
-	Intro,
-	MainMenu,
-	Exit,
-	Start,
-	Load,
-	Options,
-	Credits,
-	Graphics,
-	Sound,
-	Controls,
-	PlayGame,
-	GameOver
+	public virtual void Start() {}
+	public virtual void Update() {}
+}
+
+class MainMenuState : MenuState {
+	public override void Start()
+	{
+		SceneManager.LoadScene("MenuScene", LoadSceneMode.Single);
+	}
+}
+
+class PlayGameState : MenuState {
+	public override void Start()
+	{
+		SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+	}
+	private int tick = 0;
+	public override void Update()
+	{
+		tick++;
+		if (tick > 100)
+			MenuStateMachine.states.currentState = new GameOverState();
+	}
+}
+
+class GameOverState : MenuState
+{
+	public override void Start()
+	{
+		MenuStateMachine.states.currentState = new MainMenuState();
+	}
+}
+
+class OptionsState : MenuState
+{
+	public override void Start()
+	{
+		SceneManager.LoadScene("OptionsScene", LoadSceneMode.Single);
+	}
+}
+
+class CreditsState : MenuState
+{
+	public override void Start()
+	{
+		SceneManager.LoadScene("CreditsScene", LoadSceneMode.Single);
+	}
+}
+
+class ExitState : MenuState {
+	public override void Start()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
 }
--- Lesson3/UnityIntegration/GameOfLife/Assets/MenuStateMachine.cs
+++ Lesson3/UnityIntegration/GameOfLife/Assets/MenuStateMachine.cs
@@ -1,57 +1,41 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuStateMachine : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+//Different ways to work with state machines:
+// 1. if/else mess
+// 2. dictionary with enums
+// 3. dictionary or lists with actions
+// 4. class based (used here via MenuState classes)
+public class MenuStateMachine : MonoBehaviour
+{
+	void Start()
+	{
+		states = this;
+		currentState = new MainMenuState();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update()
+	{
+		currentState.Update();
 	}
 
-	MenuState currentState = MenuState.MainMenu;
-	
+	public static MenuStateMachine states;
+	public MenuState currentState = new MainMenuState();
+
 	public void ChangeState(string newState)
 	{
-		MenuState state = (MenuState)Enum.Parse(typeof(MenuState), newState);
-		if (currentState == MenuState.MainMenu)
-		{
-			if (state == MenuState.Start || state == MenuState.Load || state == MenuState.Options ||
-					state == MenuState.Credits || state == MenuState.Exit)
-				currentState = MenuState.Start;
-			else
-				throw new NotSupportedException("State " + state + " is not allowed in " + currentState);
-		}
-		//other screens
-		ExecuteState(state);
-	}
-
-	private void ExecuteState(MenuState state)
-	{
-		switch (state)
-		{
-		case MenuState.MainMenu:
-			//Load MainMenu scene
-			break;
-		case MenuState.PlayGame:
-			//Load GameScene
-			break;
-		case MenuState.GameOver:
-			//100 Ticks passed, go back to MainMenu
-			break;
-		case MenuState.Exit:
-#if UNITY_EDITOR
-			UnityEditor.EditorApplication.isPlaying = false;
-#else
-			Application.Quit();
-#endif
-			break;
-		}
+		if (newState == "MainMenu")
+			currentState = new MainMenuState();
+		else if (newState == "Start" || newState == "Load")
+			currentState = new PlayGameState();
+		else if (newState == "Options")
+			currentState = new OptionsState();
+		else if (newState == "Credits")
+			currentState = new CreditsState();
+		else if (newState == "Exit")
+			currentState = new ExitState();
+		else
+			throw new NotSupportedException("State change to " + newState + " is not supported yet!");
+		currentState.Start();
 	}
 }
