bf1d93139a678a48e391955b06c560f8d605f43b
Jannis Kohlstrung <38348933+SirJannis@users.noreply.github.com>
2018-06-13 10:09:35 +0200

    Building a very small wall

--- Cpp/FPS/FPSGame.cpp
+++ Cpp/FPS/FPSGame.cpp
@@ -7,26 +7,123 @@ FPSGame::FPSGame() : Game("FPS Game")
 {
 	groundTexture = std::make_shared<Texture>("Ground.png");
 	wallTexture = std::make_shared<Texture>("Wall.png");
+	aGroundVertices[0] = VertexPositionUV(-10, -10, 0, 0, 0);
+	aGroundVertices[1] = VertexPositionUV(10, -10, 0, 20, 0);
+	aGroundVertices[2] = VertexPositionUV(10, 10, 0, 20, 20);
+	aGroundVertices[3] = VertexPositionUV(-10, -10, 0, 0, 0);
+	aGroundVertices[4] = VertexPositionUV(10, 10, 0, 20, 20);
+	aGroundVertices[5] = VertexPositionUV(-10, 10, 0, 0, 20);
+
+	//Front
+	aWallVertices[0] = VertexPositionUV(0, 0, 0, 0, 0);
+	aWallVertices[1] = VertexPositionUV(0, 0, 1, 0, 1);
+	aWallVertices[2] = VertexPositionUV(0, 1, 1, 1, 1);
+	aWallVertices[3] = VertexPositionUV(0, 0, 0, 0, 0);
+	aWallVertices[4] = VertexPositionUV(0, 1, 1, 1, 1);
+	aWallVertices[5] = VertexPositionUV(0, 1, 0, 1, 0);
+
+	//Up
+	aWallVertices[6] = VertexPositionUV(0, 0, 1, 0, 0);
+	aWallVertices[7] = VertexPositionUV(1, 0, 1, 0, 1);
+	aWallVertices[8] = VertexPositionUV(1, 1, 1, 1, 1);
+	aWallVertices[9] = VertexPositionUV(0, 0, 1, 0, 0);
+	aWallVertices[10] = VertexPositionUV(1, 1, 1, 1, 1);
+	aWallVertices[11] = VertexPositionUV(0, 1, 1, 1, 0);
+
+	//Back
+	aWallVertices[12] = VertexPositionUV(1, 1, 1, 0, 0);
+	aWallVertices[13] = VertexPositionUV(1, 1, 0, 0, 1);
+	aWallVertices[14] = VertexPositionUV(1, 0, 0, 1, 1);
+	aWallVertices[15] = VertexPositionUV(1, 1, 1, 0, 0);
+	aWallVertices[16] = VertexPositionUV(1, 0, 0, 1, 1);
+	aWallVertices[17] = VertexPositionUV(1, 0, 1, 1, 0);
+
+	//Right
+	aWallVertices[18] = VertexPositionUV(0, 0, 0, 0, 0);
+	aWallVertices[19] = VertexPositionUV(0, 0, 1, 0, 1);
+	aWallVertices[20] = VertexPositionUV(1, 0, 1, 1, 1);
+	aWallVertices[21] = VertexPositionUV(0, 0, 0, 0, 0);
+	aWallVertices[22] = VertexPositionUV(1, 0, 1, 1, 1);
+	aWallVertices[23] = VertexPositionUV(1, 0, 0, 1, 0);
+
+	//Left
+	aWallVertices[24] = VertexPositionUV(0, 1, 0, 0, 0);
+	aWallVertices[25] = VertexPositionUV(0, 1, 1, 0, 1);
+	aWallVertices[26] = VertexPositionUV(1, 1, 1, 1, 1);
+	aWallVertices[27] = VertexPositionUV(0, 1, 0, 0, 0);
+	aWallVertices[28] = VertexPositionUV(1, 1, 1, 1, 1);
+	aWallVertices[29] = VertexPositionUV(1, 1, 0, 1, 0);
 }
 
 void FPSGame::PlayGame()
 {
 	Run([=]()
 	{
+		KeyPressReactions();
+		//Drawing the Sky
 		glClearColor(49 / 255.0f, 90 / 255.0f, 137 / 255.0f, 1);
-		//Call GameLogic
-		//Call RenderCode
+		SetUpProjectionMatrix(1, 100, 60);
+		SetUpCamera();
+		DrawGround();
+		DrawWall();
 	});
 }
 
-void FPSGame::SetUpProjectionMatrix(float nearPlane, float farPlane, float myFov)
+void FPSGame::SetUpProjectionMatrix(float nearPlane, float farPlane, float fov)
 {
-	GLfloat zNear = nearPlane;
-	GLfloat zFar = farPlane;
-	GLfloat aspect = (float)viewportWidth / (float)viewportHeight;
-	GLfloat fov = myFov;
-	GLfloat fH = tan(float(fov / 360.0f*3.14159f)) * zNear;
-	GLfloat fW = fH * aspect;
-	glFrustum(-fW, fW, -fH, fH, zNear, zFar);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	float aspect = (float)viewportWidth / (float)viewportHeight;
+	float fovHeight = tan(float(fov / 360.0f*3.14159f)) * nearPlane;
+	float fovWidth = fovHeight * aspect;
+	glFrustum(-fovWidth, fovWidth, -fovHeight, fovHeight, nearPlane, farPlane);
+}
 
+void FPSGame::SetUpCamera()
+{
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glRotatef(270, 1, 0, 0);
+	glRotatef(-headingInDegree, 0, 0, 1);
+	glTranslatef(playerPositionX, playerPositionY, -1.8f);
+}
+
+void FPSGame::DrawGround()
+{
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, groundTexture->GetHandle());
+	glBegin(GL_TRIANGLES);
+	for (int v = 0; v < sizeof(aGroundVertices) / VertexPositionUV::SizeInBytes; v++)
+	{
+		aGroundVertices[v].Draw();
+	}
+	glEnd();
+}
+
+void FPSGame::DrawWall()
+{
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, wallTexture->GetHandle());
+	glBegin(GL_TRIANGLES);
+	for (int v = 0; v < sizeof(aWallVertices) / VertexPositionUV::SizeInBytes; v++)
+	{
+		aWallVertices[v].Draw();
+	}
+	glEnd();
+}
+
+void FPSGame::KeyPressReactions()
+{
+	const float cDegreeToRadiant = 3.14159 / 180.0f;
+	float movement = 0;
+	if (leftPressed)
+		headingInDegree += GetTimeDelta() * 90;
+	else if (rightPressed)
+		headingInDegree -= GetTimeDelta() * 90;
+	if (upPressed)
+		movement = 1;
+	else if (downPressed)
+		movement = -1;
+	playerPositionX += sin(headingInDegree * cDegreeToRadiant) * GetTimeDelta() * 5 * movement;
+	playerPositionY += cos(headingInDegree * cDegreeToRadiant) * GetTimeDelta() * 5 * -movement;
 }
--- Cpp/FPS/FPSGame.h
+++ Cpp/FPS/FPSGame.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Game.h"
 #include <../glfw-3.2.1.bin.WIN64/include/GLFW/glfw3.h>
+#include "VertexPositionUV.h"
 using namespace CppGameEngine;
 
 class FPSGame : public Game
@@ -9,8 +10,18 @@ public:
 	FPSGame();
 	void PlayGame();
 	void SetUpProjectionMatrix(float nearPlane, float farPlane, float fov);
+	void DrawGround();
+	void DrawWall();
+	void SetUpCamera();
+	void KeyPressReactions();
 
 private:
 	std::shared_ptr<Texture> groundTexture;
 	std::shared_ptr<Texture> wallTexture;
+	VertexPositionUV aGroundVertices[6];
+	VertexPositionUV aWallVertices[36];
+
+	float playerPositionX = 0;
+	float playerPositionY = 0;
+	float headingInDegree = 0;
 };
