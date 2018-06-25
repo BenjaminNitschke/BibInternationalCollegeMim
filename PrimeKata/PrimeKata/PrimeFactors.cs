76142d52f23d048d35e98543dfc2521e3a1b667f
Kamil Muzyka <38417618+Enneari@users.noreply.github.com>
2018-06-25 10:10:36 +0200

    Add files via upload

---
+++ PrimeKata/PrimeKata/PrimeFactors.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFactorsKata
+{
+    namespace PrimeKata
+    {
+        public static class PrimeFactors
+        {
+            public static List<int> Primes(int n)
+            {
+                return new List<int> { 1 };
+            }
+
+            public static bool IsPrime(int number)
+            {
+                List<int> knownPrimes = new List<int> { 1, 2 };
+                int counter = 3;
+
+                while (number > counter)
+                {
+                    for (int i = 1; i < knownPrimes.Count; i++)
+                        if (number % knownPrimes[i] = 0)
+                            return false;
+                    knownPrimes.Add(counter);
+                    counter++;
+                }
+                return true;
+            }
+
+            public static List<int> Generate(int n)
+            {
+                return new List<int> { 1 };
+            }
+        }
+    }
+}
+
+
---
+++ PrimeKata/PrimeKata/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrimeFactorsKata.PrimeKata;
+
+namespace PrimeKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IsPrime(1)=" + PrimeFactors.IsPrime(1));
+            Console.WriteLine("IsPrime(3)=" + PrimeFactors.IsPrime(3));
+            Console.WriteLine("IsPrime(4)=" + PrimeFactors.IsPrime(4));
+
+            List<int> n = PrimeFactors.Primes(10);
+            Console.Write(n + ", ");
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Generate(" + i + ")=" + String.Join("*",
+                PrimeFactors.Generate(i)));
+            }
+        }
+    }
+}
