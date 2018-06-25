9a1d430983fa83d3793559579886e7380d39d354
NiegschKannEr <38417797+Energising@users.noreply.github.com>
2018-06-25 09:59:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIM_Test
+{
+    class PrimeFactors
+    {
+        public static List<int> GetPrimeNumbers(int numberOfPrimes)
+        {
+            return new List<int> { 1 };
+        }
+
+        public static bool IsPrime(int number)
+        {
+            var knownPrimes = new List<int> { 1, 2 };
+            int counter = 3;
+            while(number > counter)
+            {
+                for (int i = 1; i < knownPrimes.Count; i++)
+                    if (number % knownPrimes[i] == 0)
+                        return false;
+                knownPrimes.Add(counter);
+                counter++;
+            }
+            return true;
+        }
+    }
+}
