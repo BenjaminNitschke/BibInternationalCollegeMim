510fb997b5fa0a714ffcbe40f3ec262f9edafda1
Philipp <38353072+JanPhilippSchwefer@users.noreply.github.com>
2018-06-25 09:32:15 +0200

    PrimeFactors

---
+++ PrimeFactors/PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace PrimeFactors
+{
+    public class PrimeFactors
+    {
+        // Generates a given amount of prime numbers.
+        public static List<int> Generate(int amount)
+        {
+            List<int> primeNumbers = new List<int>();
+            int i = 2;
+
+            while (primeNumbers.Count < amount)
+            {
+                bool isPrimeNumber = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrimeNumber = false;
+                        break;
+                    }
+                }
+                if (isPrimeNumber)
+                {
+                    primeNumbers.Add(i);
+                }
+                i++;
+            }
+
+            return primeNumbers;
+        }
+        
+        // Generates all prime factors of a given number.
+        public static List<int> GetPrimeFactors(int number)
+        {
+            List<int> primeFactors = new List<int>();
+
+            int x = GetNextPrimeFactor(number);
+            if (x == 0)
+            {
+                Console.WriteLine(number + " is a prime number");
+                return primeFactors;
+            }
+
+            while(x != 0)
+            {
+                primeFactors.Add(x);
+                number /= x;
+                x = GetNextPrimeFactor(number);
+            }
+            primeFactors.Add(number);
+
+            return primeFactors;
+        }
+
+        // Calculates the next prime factor of a given number.
+        private static int GetNextPrimeFactor(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        // Generates all prime numbers up to the given number.
+        public static List<int> GeneratWithMaxNumber(int maxNumber)
+        {
+            List<int> primeNumbers = new List<int>();
+            for (int i = 2; i <= maxNumber; i++)
+            {
+                bool isPrimeNumber = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrimeNumber = false;
+                        break;
+                    }
+                }
+                if (isPrimeNumber)
+                {
+                    primeNumbers.Add(i);
+                }
+            }
+
+            return primeNumbers;
+        }
+
+    }
+}
---
+++ PrimeFactors/PrimeFactors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFactors
+{
+    public class Program
+    {
+        private static List<int> primeNumbers;
+        private static List<int> primeFactors;
+
+        private static int amountOfPrimeNumbers = 10;
+        private static int numberToFactorize = 27;
+
+        static void Main(string[] args)
+        {
+            primeNumbers = PrimeFactors.Generate(amountOfPrimeNumbers);
+
+            Console.WriteLine("The first {0} Prime Numbers are:", amountOfPrimeNumbers);
+            foreach (int i in primeNumbers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            primeFactors = PrimeFactors.GetPrimeFactors(numberToFactorize);
+
+            if (primeFactors.Count > 0)
+            {
+                Console.WriteLine("The Prime Factors are:");
+                foreach (int factor in primeFactors)
+                {
+                    Console.WriteLine(factor);
+                }
+            }
+        }
+    }
+}
