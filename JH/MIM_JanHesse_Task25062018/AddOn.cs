a2db56fa776cec15ad5cf4cfeaeaad76c154a183
JanHesse <38418521+JanHesse@users.noreply.github.com>
2018-06-25 09:40:05 +0200

    Add files via upload

---
+++ JH/MIM_JanHesse_Task25062018/AddOn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIM_PrimeFactorsProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> Try = Program.generate(9);
+            foreach (int t in Try)
+            {
+                Console.WriteLine(t);
+            }
+            Console.ReadLine();
+        }
+     }
+}
---
+++ JH/MIM_JanHesse_Task25062018/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIM_PrimeFactors
+{
+    public static class PrimeFactors
+    {
+        public static List<int> generate(int Zahl)
+        {
+            List<int> PrimeFactors = new List<int>();
+            double sqrt = Math.Sqrt(Zahl);
+            double ueber = sqrt % 2;
+            if (IsPrimeFactor(Zahl))
+            {
+                PrimeFactors.Add(Zahl);
+                return PrimeFactors;
+            }
+            Console.WriteLine(ueber);
+            Console.ReadLine();
+            return null;
+        }
+        public static bool IsPrimeFactor(int zl)
+        {
+            for (int i = 2; i < zl, i++)
+            {
+                double ueber = zl% i;
+                if (ueber == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
