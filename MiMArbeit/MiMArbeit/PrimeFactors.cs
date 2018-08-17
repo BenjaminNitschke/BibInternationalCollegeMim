bfba34f0dae7f5ab2e93f67c21acbf2bfed597e6
Tom Last <38315413+TomLast@users.noreply.github.com>
2018-08-17 12:43:14 +0200

    MiM Arbeit Tom Last

---
+++ MiMArbeit/MiMArbeit/PrimeFactors.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiMArbeit
+{
+    internal class PrimeFactors
+    {
+        public static bool IsPrime(int x)
+        {
+            for (int i = 2; i < x; i++)
+            {
+                if (x % i == 0) return false;
+            }
+            return true;
+        }
+        public static List<int> Generate(int x)
+        {
+            List<int> result = new List<int>();
+            int tmp = 0;
+
+
+            if (IsPrime(x)) result.Add(x);
+            else
+            {
+                    tmp = x / 2;
+
+                    if (IsPrime(tmp))
+                        for (int j = 2; j < x; j++)
+                        {
+                            if (j * tmp == x && IsPrime(j))
+                            {
+                                result.Add(j);
+                                result.Add(tmp);
+                                return result;
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                
+            }
+            return result;
+        }
+       
+    }
+}
---
+++ MiMArbeit/MiMArbeit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiMArbeit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2=" + string.Join("*", PrimeFactors.Generate(2)));
+            Console.WriteLine("4=" + string.Join("*", PrimeFactors.Generate(4)));
+            Console.WriteLine("5=" + string.Join("*", PrimeFactors.Generate(5)));
+            Console.WriteLine("6=" + string.Join("*", PrimeFactors.Generate(6)));
+            Console.WriteLine("8=" + string.Join("*", PrimeFactors.Generate(8)));
+            //Console.WriteLine("9=" + string.Join("*", PrimeFactors.Generate(9)));
+            //Console.WriteLine("10=" + string.Join("*", PrimeFactors.Generate(10)));            
+        }     
+    }
+}
